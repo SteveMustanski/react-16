bccaa9c997ad3b761db8fae65c1f885d7ebca6b6
Steve Mustanski <steve.mustanski@gmail.com>
2018-12-16 18:23:01 -0500

    added componentDidCatch to catch error and display error message

--- src/App.js
+++ src/App.js
@@ -5,18 +5,31 @@ import './App.css';
 import StudentForm from './components/StudentForm';
 
 class App extends Component {
+
+  state = {
+    hasError: false
+  }
+
+  componentDidCatch() {
+    this.setState({hasError: true});
+  }
   
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+    if (this.state.hasError) {
+      return <h1>Oh Noes!  Something went wrong</h1>
+    } else {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+  
+          <StudentForm />
+        </div>
+      );
 
-        <StudentForm />
-      </div>
-    );
+    }
   }
 }
 
